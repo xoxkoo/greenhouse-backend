06553aa2f7bf2c49bcb9778888e89c6135fa129e
xoxkoo <anton.durcak22@gmail.com>
2023-04-04 16:49:40 +0200

    humidity dao

--- EfcDataAccess/DAOs/CO2EfcDao.cs
+++ EfcDataAccess/DAOs/CO2EfcDao.cs
@@ -1,4 +1,8 @@
 using Application.DaoInterfaces;
+using Domain.DTOs;
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace EfcDataAccess.DAOs;
 
 public class CO2EfcDao : ICO2Dao
@@ -8,5 +12,17 @@ public class CO2EfcDao : ICO2Dao
 	public CO2EfcDao(Context context)
 	{
 		_context = context;
+	}
+
+	public async Task<IEnumerable<HumidityDto>> GetHumidityAsync(SearchMeasurementDto searchMeasurement)
+	{
+		IEnumerable<Humidity> list = await _context.Humidities.ToListAsync();
+
+		return list.Select(h => new HumidityDto
+		{
+			Date = h.Date,
+			Value = h.Value,
+			HumidityId = h.HumidityId
+		});
 	}
 }
