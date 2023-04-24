8ada6cf08211e2be12e46d1672a17f486ad430fa
Jakub babica <315190@via.dk>
2023-04-24 13:49:57 +0200

    controller

---
+++ WebAPI/Controllers/WateringSystemController.cs
@@ -0,0 +1,32 @@
+﻿using Application.DaoInterfaces;
+using Domain.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WateringSystemController:ControllerBase
+{
+    private readonly IWateringSystemLogic Logic;
+
+    public WateringSystemController(IWateringSystemLogic logic)
+    {
+        Logic = logic;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<ValveStateDto>> PostAsync([FromBody] bool current, [FromBody] int time)
+    {
+        try
+        {
+            ValveStateDto valveStateDto = new ValveStateDto();
+            return Ok(valveStateDto);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return StatusCode(500, e.Message);
+        }
+    }
+}
