3357d73bc2e18d9c6efa1b47b97ad99da4cfd470
Jakub babica <315190@via.dk>
2023-04-26 21:49:27 +0200

    watering system dao

---
+++ EfcDataAccess/DAOs/WateringSystemDao.cs
@@ -0,0 +1,35 @@
+﻿using Application.DaoInterfaces;
+using Domain.DTOs;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace EfcDataAccess.DAOs;
+
+public class WateringSystemDao : IWateringSystemDao
+{
+    private readonly Context _context;
+
+    public WateringSystemDao(Context context)
+    {
+        _context = context;
+    }
+
+    public async Task<ValveStateDto> CreateAsync(ValveState valveState)
+    {
+        EntityEntry<ValveState> entity = await _context.ValveState.AddAsync(valveState);
+        await _context.SaveChangesAsync();
+        ValveStateDto dto = new ValveStateDto()
+        {
+            Toggle = entity.Entity.Toggle
+        };
+        return dto;
+    }
+
+    public async Task<ValveStateDto> GetAsync()
+    {
+        IQueryable<ValveState> tempQuery= _context.ValveState.AsQueryable();
+        ValveStateDto result = tempQuery.Select(v => new ValveStateDto() { Toggle = v.Toggle }).FirstOrDefault();
+        return result;
+        //missing await
+    }
+}
