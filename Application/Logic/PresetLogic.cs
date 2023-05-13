9e110316b6b854c30661abc4912c3cbbd7eaf8cd
Mateusz Zbyslaw <315192@viauc.dk>
2023-05-13 15:05:11 +0200

    Feat  57 - Finished logic for preset create

--- Application/Logic/PresetLogic.cs
+++ Application/Logic/PresetLogic.cs
@@ -18,51 +18,12 @@ public class PresetLogic : IPresetLogic
     {
         return await _presetDao.GetAsync(dto);
     }
-
     public async Task<PresetEfcDto> CreateAsync(PresetCreationDto dto)
     {
-        if (dto == null)
-        {
-            throw new ArgumentNullException(nameof(dto), "Provided data cannot be null");
-        }
-        if (dto.Thresholds == null || dto.Thresholds.Count() != 3)
-        {
-            throw new ArgumentException("Exactly three thresholds must be provided", nameof(dto.Thresholds));
-        }
-        List<Threshold> thresholds = dto.Thresholds.Select(t => new Threshold
-        {
-            Type = t.Type,
-            MaxValue = t.MaxValue,
-            MinValue = t.MinValue
-        }).ToList();
+        ValidateInput(dto);
 
-        foreach (var threshold in thresholds)
-        {
-            Console.WriteLine(threshold.Type);
-            /*if (!threshold.Type.Equals("CO2") || !threshold.Type.Equals("Temperature") ||
-                !threshold.Type.Equals("Humidity"))
-            {
-                throw new ArgumentException("Threshold type must be either: CO2, Humidity or Temperature.");
-            }*/
-
-            if (threshold.MaxValue < threshold.MinValue)
-            {
-                throw new ArgumentException("Min value cannot be bigger then max value.");
-            }
-
-            if (threshold.Type.Equals("CO2") && !(threshold.MinValue >= 0 && threshold.MaxValue <= 4095))
-            {
-                throw new ArgumentException("CO2 value is randing from 0 to 4095");
-            }
-            if (threshold.Type.Equals("Humidity") && !(threshold.MinValue >= 0 && threshold.MaxValue <= 100))
-            {
-                throw new ArgumentException("Humidity value is randing from 0 to 100");
-            }
-            if (threshold.Type.Equals("Temperature") && !(threshold.MinValue >= -50 && threshold.MaxValue <= 60))
-            {
-                throw new ArgumentException("Temperature value is randing from -50 to 60");
-            }
-        }
+        List<Threshold> thresholds = MapThresholds(dto.Thresholds);
+        ValidateThresholds(thresholds);
 
         Preset preset = new Preset
         {
@@ -72,5 +33,107 @@ public class PresetLogic : IPresetLogic
         };
 
         return await _presetDao.CreateAsync(preset);
+    }
+
+    private void ValidateInput(PresetCreationDto dto)
+    {
+        if (dto == null)
+        {
+            throw new ArgumentNullException(nameof(dto), "Provided data cannot be null");
+        }
+        if (dto.Thresholds == null || dto.Thresholds.Count() != 3)
+        {
+            throw new ArgumentException("Exactly three thresholds must be provided");
+        }
+    }
+
+    private List<Threshold> MapThresholds(IEnumerable<ThresholdDto> thresholdDtos)
+    {
+        List<Threshold> thresholds = thresholdDtos.Select(t => new Threshold
+        {
+            Type = t.Type,
+            MaxValue = t.MaxValue,
+            MinValue = t.MinValue
+        }).ToList();
+
+        if (HasDuplicateThresholdTypes(thresholds))
+        {
+            throw new ArgumentException("There must be exactly three thresholds named: CO2, Humidity, and Temperature.");
+        }
+
+        return thresholds;
+    }
+
+    private bool HasDuplicateThresholdTypes(List<Threshold> thresholds)
+    {
+        var uniqueTypes = new HashSet<string>();
+        foreach (var threshold in thresholds)
+        {
+            if (!uniqueTypes.Add(threshold.Type.ToLower()))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void ValidateThresholds(List<Threshold> thresholds)
+    {
+        foreach (var threshold in thresholds)
+        {
+            NormalizeThresholdType(threshold);
+
+            if (threshold.MaxValue < threshold.MinValue)
+            {
+                throw new ArgumentException("Min value cannot be bigger than max value.");
+            }
+
+            if (!IsValidThresholdRange(threshold))
+            {
+                throw new ArgumentException(GetThresholdRangeErrorMessage(threshold));
+            }
+        }
+    }
+
+    private void NormalizeThresholdType(Threshold threshold)
+    {
+        if (threshold.Type.ToLower() == "co2")
+        {
+            threshold.Type = "CO2";
+        }
+        else
+        {
+            threshold.Type = char.ToUpperInvariant(threshold.Type[0]) + threshold.Type.Substring(1).ToLowerInvariant();
+        }
+    }
+
+    private bool IsValidThresholdRange(Threshold threshold)
+    {
+        switch (threshold.Type)
+        {
+            case "CO2":
+                return threshold.MinValue >= 0 && threshold.MaxValue <= 4095;
+            case "Humidity":
+                return threshold.MinValue >= 0 && threshold.MaxValue <= 100;
+            case "Temperature":
+                return threshold.MinValue >= -50 && threshold.MaxValue <= 60;
+            default:
+                return false;
+        }
+    }
+
+    private string GetThresholdRangeErrorMessage(Threshold threshold)
+    {
+        switch (threshold.Type)
+        {
+            case "CO2":
+                return "CO2 value must range from 0 to 4095.";
+            case "Humidity":
+                return "Humidity value must range from 0 to 100.";
+            case "Temperature":
+                return "Temperature value must range from -50 to 60.";
+            default:
+                return "Invalid threshold type.";
+        }
     }
 }
