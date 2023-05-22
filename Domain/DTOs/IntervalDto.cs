5dadfb00edfdf468f566d36ee19d7197df0a4fb6
Natalia <koziarkka@gmail.com>
2023-05-22 13:01:53 +0200

    Bug - 70 - Commiting the IntervalDto file

---
+++ Domain/DTOs/IntervalDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.DTOs;
+
+public class IntervalDto
+{
+    public int Id { get; init; }
+    public TimeSpan StartTime { get; set; }
+    public TimeSpan EndTime { get; set; }
+    public DayOfWeek DayOfWeek { get; set; }
+}
