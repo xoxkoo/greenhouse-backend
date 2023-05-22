35787e33ddb569fe4f4488ce29e798d0a1ba5aa3
Natalia <koziarkka@gmail.com>
2023-05-22 12:20:09 +0200

    Feat - 70 - IntervalDto added ID

--- Domain/DTOs/IntervalDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Domain.DTOs;
-
-public class IntervalDto
-{
-    public int Id { get; init; }
-    public TimeSpan StartTime { get; set; }
-    public TimeSpan EndTime { get; set; }
-    public DayOfWeek DayOfWeek { get; set; }
-}
