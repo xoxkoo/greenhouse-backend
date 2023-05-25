059b993411f317afd2a0a2d0bf2dd032de70cf5e
xoxkoo <anton.durcak22@gmail.com>
2023-05-25 10:05:42 +0200

    feat - 90 - added tests for watering system dao

--- UnitTest/DaoTests/WateringSystemDaoTest.cs
+++ UnitTest/DaoTests/WateringSystemDaoTest.cs
@@ -18,4 +18,24 @@ public class WateringSystemDaoTest : DbTestBase
         dao = new WateringSystemDao(DbContext);
     }
 
+    [TestMethod]
+    public async Task CreateAsync_ToTrue()
+    {
+	    const bool newState = true;
+	    var r = await dao.CreateAsync(new ValveState(){Toggle = newState});
+
+	    Assert.AreEqual(r.State, newState);
+    }
+
+    [TestMethod]
+    public async Task CreateAsync_ToTrue_ToFalse()
+    {
+	    const bool newState = false;
+
+	    await dao.CreateAsync(new ValveState(){Toggle = true});
+	    var r = await dao.CreateAsync(new ValveState(){Toggle = newState});
+
+	    Assert.AreEqual(r.State, newState);
+    }
+
 }
