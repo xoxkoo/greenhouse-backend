49a55c38bc2b1029161a0dd6e527f49f9da603c0
xoxkoo <anton.durcak22@gmail.com>
2023-05-26 10:01:05 +0200

    feat - 90 - change db mails

--- EfcDataAccess/DatabaseInsertion.cs
+++ EfcDataAccess/DatabaseInsertion.cs
@@ -29,8 +29,8 @@ class DatabaseInsertion
         intervals.Add(intervalWednesday);
         intervals.Add(intervalFriday);
         await _context.Intervals.AddRangeAsync(intervals);
-        
-        
+
+
         //Thresholds
         Threshold threshold1 = new Threshold() { Id = 1, Type = "temperature", MinValue = 20, MaxValue = 40 };
         Threshold threshold2 = new Threshold() { Id = 2, Type = "humidity", MinValue = 20, MaxValue = 100 };
@@ -47,7 +47,7 @@ class DatabaseInsertion
 
         //Email
         NotificationEmail notificationEmail = new NotificationEmail() { Email = "greenhousesep4@gmail.com" };
-        await _context.Mails.AddAsync(notificationEmail);
+        await _context.NotificationEmails.AddAsync(notificationEmail);
 
         //ValveState
         ValveState valveState = new ValveState() { Toggle = false };
@@ -64,7 +64,7 @@ class DatabaseInsertion
         _context.Intervals.RemoveRange(_context.Intervals);
         _context.Thresholds.RemoveRange(_context.Thresholds);
         _context.Presets.RemoveRange(_context.Presets);
-        _context.Mails.RemoveRange(_context.Mails);
+        _context.NotificationEmails.RemoveRange(_context.NotificationEmails);
         _context.ValveState.RemoveRange(_context.ValveState);
         // Call the InsertMyEntitiesAsync method
         await InsertMyEntitiesAsync(10);
